0a44fba076fb204ce6995937fb337df05f101aab
paul449 <paul_bilbatua@hotmail.com>
2024-06-05 11:46:50 -0500

    webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,23 +2,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { Template } = require('webpack');
+const { title } = require('process');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      header:'./src/js/header.js',
+      editor:'./src/js/editor.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      HtmlWebpackPlugin({
+        Template:'index.html',
+        title:'jate',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 //importing express
 const express = require('express');
-
+//applying express to this app
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('../client/dist'));
+//middleware
+app.use(express.static('../client'));
+app.use(express.static('../client/src/css'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//exporting routes folder
 require('./routes/htmlRoutes')(app);
-
+//listening through port 3000
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
